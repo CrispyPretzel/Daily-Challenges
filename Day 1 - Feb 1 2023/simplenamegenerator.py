5e4d82eef72dfa2fe9ca1a194d95ed9b71b9d420
E.J <ejpila@outlook.com>
2023-02-01 22:35:31 -0500

    first commit

---
+++ Day 1 - Feb 1 2023/simplenamegenerator.py
@@ -0,0 +1,8 @@
+print('Hello Welcome to The Simple Name Generator')
+
+user_city_name = input('What city did you grow up in? \n')
+user_pet_name = input('What is the name of your pet? \n')
+
+new_user_name = user_city_name + ' ' + user_pet_name
+
+print(f'Your new name could be {new_user_name}')
